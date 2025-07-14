49aa6868cc1ea121e56e9926364fcbe545ba3fe7
Somebro <someBroYouKnow@users.noreply.github.com>
2025-07-15 00:20:07 +0530

    reverted timer

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,5 +1,5 @@
 'use client'; // This directive marks the component for client-side rendering
-const TIME= 10; // 3 minutes
+const TIME= 180; // 3 minutes
 
 import { QuestionModal, ResultModal } from '@/components/Modal';
 import { DiceRoller } from '@/components/ui/DiceComponent/DiceComponent';
